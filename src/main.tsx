6f0fd73611d70ffa39803ade0a3d1f46744ee6e0
popu <mateitudorpop@gmail.com>
2024-08-27 22:25:48 +0300

    added simple list for main page in which you can see the drones and their status

---
+++ src/main.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './styles/droneList.css'
+
+const drones = [
+    {
+        id: 0,
+        status: "parked"
+    },
+    {
+        id: 1,
+        status: "delivering"
+    },
+    {
+        id: 2,
+        status: "returning"
+    },
+    {
+        id: 3,
+        status: "undefined"
+    }
+];
+
+function Main() {
+    let list = [];
+    for(let i = 0; i < drones.length; i++) {
+        if(drones[i].status === "parked") {
+            list[i] = <li className="green">
+                {drones[i].id} - {drones[i].status};
+            </li>
+        }
+        else if(drones[i].status === "delivering") {
+            list[i] = <li className="red">
+                {drones[i].id} - {drones[i].status};
+            </li>
+        }
+        else if(drones[i].status === "returning") {
+            list[i] = <li className="orange">
+                {drones[i].id} - {drones[i].status};
+            </li>
+        }
+        else {
+            list[i] = <li className="dark">
+                {drones[i].id} - {drones[i].status};
+            </li>
+        }
+    }
+
+    return (
+      <ol className='ol'>
+        {list}
+      </ol>
+    );
+  }
+
+export default Main;
