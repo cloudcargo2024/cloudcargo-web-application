6d6a0746691fae3d5c30c5c25a0da26e7e0cf2c6
popu <mateitudorpop@gmail.com>
2024-09-06 15:36:36 +0300

    finished drone list adding drone list to main project

--- src/components/MapAP.tsx
+++ src/components/MapAP.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
 import '../models/Drone'
@@ -69,3 +70,20 @@ function App() {
 }
 
 export default App;
+=======
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
+export default function MapAP() {
+  return (
+    <APIProvider apiKey={"AIzaSyDPFf4C-v_O2MHUX-0azevxBPBF38IBMXE"}>
+      <Map
+        style={{ width: "100vw", height: "100vh" }}
+        defaultCenter={{ lat: 22.54992, lng: 0 }}
+        defaultZoom={3}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}
+      />
+    </APIProvider>
+  );
+}
+>>>>>>> 7e65327 (finished drone list)
