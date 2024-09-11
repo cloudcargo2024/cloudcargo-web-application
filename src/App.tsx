96f799d5ea065b6fe38ce30df8513c89ac9cb0e7
horatiu.tomoiaga <horatiu.tomoiaga@fanduel.com>
2024-09-11 09:55:43 +0300

    modified code for useContext to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./styles/App.css";
-import isLoggedIncontext from  "./context/context";
+import isLoggedIncontext from "./context/context";
 import {
   createBrowserRouter,
   Route,
@@ -12,15 +12,12 @@ import Main from "./views/main";
 import LogIn from "./components/login";
 
 function App() {
-  const [isLoggedIn, setisLoggedIn] = useState(false);
   const router = createBrowserRouter(
     createRoutesFromElements(
-      // <isLoggedIncontext.Provider value={isLoggedIn}>
       <>
         <Route path="/" element={<LogIn />} />
         <Route path="/main" element={<Main />} />
-        </>
-      // </isLoggedIncontext.Provider>
+      </>
     )
   );
 
