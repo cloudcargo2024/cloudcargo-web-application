a7dfa68de674b9ea55b67b4284c2c153b2ce4905
Andrei Pavel <pavelandrei122@gmail.com>
2024-09-26 16:50:55 +0300

    fixed bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import Main from "./components/DroneSection";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>
 );
 
