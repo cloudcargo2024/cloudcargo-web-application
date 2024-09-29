785c7b87bd6ac8abe29f0f1d3b75c9f5141c5fa7
Andrei Pavel <pavelandrei122@gmail.com>
2024-09-29 11:45:16 +0300

    chestie

---
+++ src/components/DroneFlight.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+
+export default function DroneFlight() {
+  async function sendCommand(command: string) {
+    fetch(`/api/drone-flight/${command}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+  }
+
+  let isProcessing = false;
+
+  async function processEvent(command: string) {
+    if (isProcessing) return;
+    isProcessing = true;
+    try {
+      await sendCommand(command);
+    } finally {
+      isProcessing = false;
+    }
+  }
+
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+  async function sendMultipleCommands() {
+    const commands = [
+      "up",
+      "takeoff",
+      "rotateleft",
+      "rotateleft",
+      "rotateright",
+      "rotateright",
+      "stop",
+    ];
+
+    for (const command of commands) {
+      await processEvent(command); // Send the command
+      await delay(1000); // Wait for 3 seconds before sending the next command
+    }
+  }
+
+  return (
+    <>
+      <h1>Drone commands</h1>
+      <button onClick={() => sendMultipleCommands()}>Flight</button>
+      <button onClick={() => processEvent("appstart")}>Start app</button>
+      <button onClick={() => processEvent("default")}>Default</button>
+      <button onClick={() => processEvent("takeoff")}>Take off</button>
+      <button onClick={() => processEvent("stop")}>Stop</button>
+      <button onClick={() => processEvent("land")}>Land</button>
+      <button onClick={() => processEvent("up")}>Up</button>
+      <button onClick={() => processEvent("down")}>Down</button>
+      <button onClick={() => processEvent("rotateleft")}>Rotate left</button>
+      <button onClick={() => processEvent("rotateright")}>Rotate right</button>
+      <button onClick={() => processEvent("forward")}>Forward</button>
+      <button onClick={() => processEvent("back")}>Back</button>
+      <button onClick={() => processEvent("left")}>Left</button>
+      <button onClick={() => processEvent("right")}>Right</button>
+    </>
+  );
+}
